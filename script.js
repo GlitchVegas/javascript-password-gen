0b34e5e5c2d0f0ac65b6fc8bcbcd89ce78960dfd
Glitch Vegas <carterpenn115@gmail.com>
2022-09-18 07:12:14 -0500

    added comments add 4 new var
    Added Comments to tell me what I need to work on
    
    Added Vars
    lowercase, uppercase, numeric, specialCharacters

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//var for lowercase
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+//var for UPPERCASE
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//var for Numeric
+var numeric = "0123456789"
+//var for Special Characters
+var specialCharacters = "~`!@#$%^&*()_-+=" //only using the top row of symbols
+
+
+
+//Click Button
+//Prompt password Criterias
+  //Prompt is Length length between 8 & 128 Characters
+    //Prompt lowercase
+      //Prompt UPPERCASE
+        //Prompt Numeric
+          //Prompt Special Characters (!, @ ,# ,$ , ect..)
+            //Make Sure at least 1 is selected as yes before proceeding
+//Generate Random Password Based on Answers
+//Write in Generated Password into html box
