64395e473aa5c114b5fa45c7fbcb0cc4421ed2df
Glitch Vegas <carterpenn115@gmail.com>
2022-09-18 08:40:55 -0500

    Added Prompts
    Added Length, Lowercase, Uppercase, Numeric, and Special Characters Prompts

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 //var for lowercase
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 //var for UPPERCASE
@@ -22,15 +10,64 @@ var numeric = "0123456789"
 //var for Special Characters
 var specialCharacters = "~`!@#$%^&*()_-+=" //only using the top row of symbols
 
-
-
 //Click Button
-//Prompt password Criterias
+function generatePassword() {
+  //Prompt password Criterias
   //Prompt is Length length between 8 & 128 Characters
-    //Prompt lowercase
+  var pLength = prompt(" Pick a Number 8 to 128");
+  console.log(pLength);
+  
+  if ( 
+    (pLength <= 7) ||
+    (pLength >= 129) ||
+    isNaN(pLength)
+    ) {
+      alert("Please enter an Password Length Greater then 8 and Less then 128");
+      
+    } else {
+      //Prompt lowercase
+      var cLowercase = confirm("Press OK to add Lowercase.")
       //Prompt UPPERCASE
-        //Prompt Numeric
-          //Prompt Special Characters (!, @ ,# ,$ , ect..)
-            //Make Sure at least 1 is selected as yes before proceeding
+      var cUppercase = confirm("Press OK to add Uppercase.")
+      //Prompt Numeric
+      var cNumeric = confirm("Press OK to add Numeric.")
+      //Prompt Special Characters (!, @ ,# ,$ , ect..)
+      var cSpecial = confirm("Press OK to add Special Characters.")
+    }
+    console.log(cLowercase, cUppercase, cNumeric, cSpecial);
+    //Make Sure at least 1 is selected as yes before proceeding
+    if (
+      (cLowercase === false) &&
+      (cUppercase === false) &&
+      (cNumeric === false) &&
+      (cSpecial === false)
+      ) {
+        alert("Please Enter at least one choice")
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    }
+    function writePassword() { 
+    // Write password to the #password input
+    //Write in Generated Password into html box
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    
+    passwordText.value = password;
+  }
+    // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
 //Generate Random Password Based on Answers
-//Write in Generated Password into html box
